53fecf3c34e1f5f785ead24c8fbe199188d9a67d
Anna Kapanadze <anna.kapanadze@outlook.com>
2026-03-02 22:34:57 +0000

    Create mad_libs.py

---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+noun = input("Input a noun")
+verb = input("Input a verb")
+adjective = input("Input an adjective")
+place = input("Input a place")
+
+story = "One day, a " + adjective + " " + noun + " decided to " + verb + " all the way to " + place + ". Everyone was shocked!"
+
+print("\nYour story:")
+print(story)
