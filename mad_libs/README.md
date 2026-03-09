ea0d34f7780da0822c45dfd5d6b5a9e6a865ca9e
Anna Kapanadze <anna.kapanadze@outlook.com>
2026-03-09 19:45:24 +0000

    Delete mad_libs directory

--- mad_libs/README.md
+++
@@ -1,9 +0,0 @@
-# Mad Libs Generator
-
-A mini mad libs game made in Python.
-
-## What I used
-- Python
-- Variables
-- User input
-- String concatenation
--- mad_libs/mad_libs.py
+++
@@ -1,9 +0,0 @@
-noun = input("Input a noun")
-verb = input("Input a verb")
-adjective = input("Input an adjective")
-place = input("Input a place")
-
-story = "One day, a " + adjective + " " + noun + " decided to " + verb + " all the way to " + place + ". Everyone was shocked!"
-
-print("\nYour story:")
-print(story)
